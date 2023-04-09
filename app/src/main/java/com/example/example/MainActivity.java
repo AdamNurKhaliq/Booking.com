108f042f1a6d3bd167ab0366ccfa9cf28111510b
AdamNurKhaliq <adamnurkhaliq17@gmail.com>
2023-04-09 20:44:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/example/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
